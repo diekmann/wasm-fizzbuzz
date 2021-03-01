03ac200ece379f88e40c70f5c506f4376e6417f7
diekmann <none@localhost>
2021-03-01 11:06:03 +0100

    compiles and starts and segfaults.
    $ linux/linuxxdoom
    Game mode indeterminate.
                         Public DOOM - v1.10
    V_Init: allocate screens.
    M_LoadDefaults: Load system defaults.
    Z_Init: Init zone memory allocation daemon.
    W_Init: Init WADfiles.
    Error: W_InitFiles: no files found
    Segmentation fault (core dumped)

--- doom/linuxdoom-1.10/i_sound.c
+++ doom/linuxdoom-1.10/i_sound.c
@@ -28,6 +28,8 @@ rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 #include <stdlib.h>
 #include <stdarg.h>
 
+#include <errno.h>
+
 #include <math.h>
 
 #include <sys/time.h>
@@ -163,7 +165,7 @@ myioctl
   int*	arg )
 {   
     int		rc;
-    extern int	errno;
+    //extern int	errno;
     
     rc = ioctl(fd, command, arg);  
     if (rc < 0)
