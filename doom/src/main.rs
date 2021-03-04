ec7d34474c20708431bc7eddbc493ebfb767fb57
diekmann <none@localhost>
2021-03-04 21:18:06 +0100

    set argv from rust.
    DOOM decided to have them in a global variable instead of passing them as function arguments.

--- doom/src/main.rs
+++ doom/src/main.rs
@@ -1,5 +1,13 @@
+use std::ffi::CString;
+use std::os::raw::{c_char, c_int};
+
 extern "C" {
+    // d_main.c
     fn D_DoomMain() -> !;
+
+    // m_argv.c
+    static mut myargc: c_int;
+    static mut myargv: *const *const c_char;
 }
 
 fn main() {
@@ -8,5 +16,12 @@ fn main() {
     // TODO: set global variables
     // myargc=2 and myargv={"-2"}
 
-    unsafe { D_DoomMain() };
+    let binary_name = CString::new("linuxxdoom").unwrap();
+    let first_commandline_arg = CString::new("-2").unwrap();
+    let argv: [*const c_char; 2] = [binary_name.as_ptr(), first_commandline_arg.as_ptr()];
+    unsafe {
+        myargc = argv.len() as c_int;
+        myargv = &argv as *const *const c_char;
+        D_DoomMain();
+    };
 }
