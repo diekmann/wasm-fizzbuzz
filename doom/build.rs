af2746be92e963446d0ebeedccb9853540975ddd
diekmann <none@localhost>
2021-03-04 20:42:30 +0100

    tune build, run `make` from `cargo`.

--- doom/build.rs
+++ doom/build.rs
@@ -1,6 +1,16 @@
+use std::process::Command;
+
+static DOON_SRC: &str = "linuxdoom-1.10";
+
 fn main() {
-    // TODO: go to linuxdoom-1.10 and `make linux/linuxxdoom.a` first
-    println!("cargo:rustc-link-search=linuxdoom-1.10/linux");
+    println!("cargo:rerun-if-changed={}", DOON_SRC);
+
+    let status = Command::new("make").args(&["-C", DOON_SRC, "linux/liblinuxxdoom.a"]).status().expect("failed to start make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
+    }
+
+    println!("cargo:rustc-link-search={}/linux", DOON_SRC);
     println!("cargo:rustc-link-lib=linuxxdoom");
 
 
