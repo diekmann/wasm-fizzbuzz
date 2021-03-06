d37ac2d484ea64fa20936280ca94992f48a7e6ef
diekmann <none@localhost>
2021-03-06 18:42:14 +0100

    Use musllibc wasm32 headers instead of those of my machine.

--- doom/build.rs
+++ doom/build.rs
@@ -5,16 +5,6 @@ static LIBC_SRC: &str = "musl-1.2.2";
 static LIBGCC_SRC: &str = "clang_compiler_rt";
 
 fn main() {
-    // original Doom Sources
-    println!("cargo:rerun-if-changed={}", DOON_SRC);
-    let status = Command::new("make").args(&["-C", DOON_SRC, "linux/liblinuxxdoom.a"]).status().expect("failed to start doom make");
-    if !status.success(){
-        panic!("Failed to make: {}", status);
-    }
-    println!("cargo:rustc-link-search={}/linux", DOON_SRC);
-    println!("cargo:rustc-link-lib=linuxxdoom");
-
-
     // libc
     println!("cargo:rerun-if-changed={}", LIBC_SRC);
     let status = Command::new("make").args(&["-C", LIBC_SRC, "lib/libc.a"]).status().expect("failed to start musl libc make");
@@ -23,6 +13,12 @@ fn main() {
     }
     println!("cargo:rustc-link-search={}/lib/", LIBC_SRC);
     println!("cargo:rustc-link-lib=c");
+
+    // system headers for wasm32
+    let status = Command::new("make").args(&["-C", LIBC_SRC, "install-headers"]).status().expect("failed to start musl libc install-headers make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
+    }
 
 
     // compiler runtime, with e.g., floating point funtcions
@@ -33,4 +29,14 @@ fn main() {
     }
     println!("cargo:rustc-link-search={}/build", LIBGCC_SRC);
     println!("cargo:rustc-link-lib=clang_rt.builtins-wasm32");
+
+
+    // original Doom Sources
+    println!("cargo:rerun-if-changed={}", DOON_SRC);
+    let status = Command::new("make").args(&["-C", DOON_SRC, "linux/liblinuxxdoom.a"]).status().expect("failed to start doom make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
+    }
+    println!("cargo:rustc-link-search={}/linux", DOON_SRC);
+    println!("cargo:rustc-link-lib=linuxxdoom");
 }
