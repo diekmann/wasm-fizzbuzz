e17ac6bb4990ef45485c29d733c6597f3667f665
diekmann <none@localhost>
2021-03-08 14:39:45 +0100

    alos print hello world from rust without issue.

--- doom/printf_test/src/main.rs
+++ doom/printf_test/src/main.rs
@@ -2,6 +2,23 @@ extern "C" {
     fn c_main();
 }
 
+// JavaScript
+extern "C" {
+    fn console_log(ptr: *const u8, len: usize);
+}
+
+
+macro_rules! log {
+    ($($arg:tt)*) => {
+        let __the_log_str = format!( $( $arg )* );
+        unsafe { console_log(__the_log_str.as_ptr(), __the_log_str.len()) }
+    }
+}
+
+macro_rules! println { ($($arg:tt),*) => { log!( $( $arg )* ) }; }
+macro_rules! print { ($($arg:tt),*) => { log!( $( $arg )* ) }; }
+
+
 fn main() {
     println!("Hello, world from rust!");
     unsafe { c_main(); }
